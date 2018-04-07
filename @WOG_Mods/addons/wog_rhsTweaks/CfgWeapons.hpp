4962a43d3134a6c471ef4e6926ad7fb71a51e934
Liquid <sewergun@gmail.com>
2018-04-07 19:10:49 +0300

    RHS Tweaks Init commit
    Init commit + Removed dynamic zoom on rhssaf_zrak_rd7j

---
+++ @WOG_Mods/addons/wog_rhsTweaks/CfgWeapons.hpp
@@ -0,0 +1,14 @@
+class CfgWeapons
+{
+    class Binocular;
+    class rhssaf_zrak_rd7j: Binocular
+    {
+        opticsZoomMin = 0.0357;
+        opticsZoomMax = 0.0357;
+        opticsZoomInit = 0.0357;
+        // opticsZoomMin = 0.03125;
+        // opticsZoomMax = 0.0625;
+        // opticsZoomInit = 0.0625;
+        descriptionShort = "Magnification: 7x";
+    };
+};
---
+++ @WOG_Mods/addons/wog_rhsTweaks/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+    class
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"rhssaf_c_weapons"};
+        authors[] = {"Liquid"};
+        url = "https://wogames.info";
+    };
+};
+
+#include "CfgWeapons.hpp"
